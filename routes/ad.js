0c3692d9cb24e03a8a0c96636ccffe10b08bd8ce
Truong Phap <truongphapdh@gmail.com>
2020-12-15 00:58:30 +0700

    add role into ad

--- routes/ad.js
+++ routes/ad.js
@@ -3,7 +3,11 @@ const adRoutes = express.Router();
 const adController = require('../controllers/adController');
 const auth = require('../middleware/auth');
 
-adRoutes.route('/create').post(adController.create);
+adRoutes
+  .route('/create')
+  .post(auth.authentication, auth.permit('admin'), adController.create);
 adRoutes.route('/load').get(adController.loadAllAd);
-adRoutes.route('/delete').post(adController.delete);
+adRoutes
+  .route('/delete')
+  .post(auth.authentication, auth.permit('admin'), adController.delete);
 module.exports = adRoutes;
